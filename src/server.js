01803f38f150337811dd7386b5fec4795632b2de
nich.otieno <nich.otieno@gmail.com>
2024-12-11 00:56:54 +0300

    Adds API endpoint for adding data
    Introduces a new POST endpoint '/add' to handle incoming data.  This endpoint accepts JSON data in the request body and returns a success status along with the received data.  The `http-status-codes` package has been added to provide standardized status codes.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 import express from "express";
+import { StatusCodes } from "http-status-codes";
+
 
 const app = express();
 const PORT = 3000;
+const STATUS = {
+  SUCCESS: "success",
+  ERROR: "error",
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/add", (req, res) => {
+  const data = [];
+  data.push(req.body);
+  res.status(StatusCodes.CREATED).send({
+    status: STATUS.SUCCESS,
+    data,
+  });
+
 });
 
 app.listen(PORT, () => {
